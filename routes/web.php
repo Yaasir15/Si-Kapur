74d36db600d97f9e2a883ca6978847a9b0108f33
Muhammad Yaasir <yaasir1624@gmail.comm>
2024-11-01 17:33:48 +0700

    Add New Menu

--- routes/web.php
+++ routes/web.php
@@ -7,5 +7,13 @@ use Illuminate\Support\Facades\Route;
 // });
 
 Route::get('/', function () {
+    return view('dashboard');
+});
+
+Route::get('/hitung', function () {
+    return view('hitung');
+});
+
+Route::get('/default', function () {
     return view('dashboards/default');
 });
